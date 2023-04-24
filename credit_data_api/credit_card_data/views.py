3bceb6665545431404eed2332920f282a2d3adb6
lucas1606 <lucas.almeida1606@gmail.com>
2023-04-24 20:16:54 -0300

    corected views functions names

--- credit_data_api/credit_card_data/views.py
+++ credit_data_api/credit_card_data/views.py
@@ -15,14 +15,14 @@ def getCreditCards(request):
 
 #get Credit Card
 @api_view(['GET'])
-def getUser(request, pk):
+def getCreditCard(request, pk):
     creditcard = CreditCard.objects.get(id=pk)
     serializer = CreditCardSerializer(creditcard, many=False)
     return Response(serializer.data)
 
 #add Credit Card
 @api_view(['POST'])
-def addUser(request):
+def addCreditCard(request):
     serializer = CreditCardSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
@@ -30,7 +30,7 @@ def addUser(request):
 
 #update Credit Cards
 @api_view(['PUT'])
-def updateUser(request,pk):
+def updateCreditCard(request,pk):
     creditcard = CreditCard.objects.get(id=pk)
     serializer = CreditCardSerializer(instance=creditcard, data=request.data)
 
@@ -41,7 +41,7 @@ def updateUser(request,pk):
 
 #delete Credit Cards
 @api_view(['DELETE'])
-def deleteUser(request, pk):
+def deleteCreditCard(request, pk):
     creditcard = CreditCard.objects.get(id=pk)
     creditcard.delete()
 
