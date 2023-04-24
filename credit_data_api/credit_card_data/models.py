de905aa3976ed909513455e44b9b19f46fe1ba64
lucas1606 <lucas.almeida1606@gmail.com>
2023-04-24 19:49:09 -0300

    Created Credit Card Model

--- credit_data_api/credit_card_data/models.py
+++ credit_data_api/credit_card_data/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class CreditCard(models.Model):
+    id = models.CharField(max_lenth=50)
+    ui = models.CharField(max_lenth=50)
+    Number = models.CharField(max_lenth=50)
+    expiryData = models.CharField(max_lenth=50)
+    type = models.CharField(max_lenth=50)
